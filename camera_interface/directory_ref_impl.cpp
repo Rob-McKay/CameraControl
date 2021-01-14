6fadbfd33479a77775cf7131944eac3991cff257
Rob McKay <rob@4mation.co.uk>
2021-01-14 14:25:59 +0000

    Added listing of files

--- camera_interface/directory_ref_impl.cpp
+++ camera_interface/directory_ref_impl.cpp
@@ -13,10 +13,10 @@
 #endif
 #endif
 
-#include <cstdio>
-#include <iostream>
 #include "camera_interface.hpp"
 #include "camera_interface_impl.hpp"
+#include <cstdio>
+#include <iostream>
 
 #include "EDSDK.h"
 
@@ -24,24 +24,77 @@
 
 namespace implementation
 {
-    impl_directory_ref::impl_directory_ref(EdsDirectoryItemRef r) : ref(r), file_size(0), format(0), is_folder(false), group_id(0)
-    {
-        EdsDirectoryItemInfo item;
+impl_directory_ref::impl_directory_ref(EdsDirectoryItemRef r)
+    : ref(r)
+    , file_size(0)
+    , format(0)
+    , is_folder(false)
+    , group_id(0)
+    , count(0)
+{
+    EdsDirectoryItemInfo item;
 
-        if (auto err = EdsGetDirectoryItemInfo(ref.get_ref(), &item); err != EDS_ERR_OK)
+    if (auto err = EdsGetDirectoryItemInfo(ref.get_ref(), &item); err != EDS_ERR_OK)
+    {
+        Poco::Logger::get("directory_item").error("Failed to get directory item info (%lu)", err);
+        throw eds_exception("Failed to get directory item info", err, __FUNCTION__);
+    }
+
+    file_size = item.size;
+    format = item.format;
+    name = item.szFileName;
+    is_folder = item.isFolder;
+    group_id = item.groupID;
+
+    if (is_folder)
+    {
+        EdsUInt32 listCount = 0;
+        if (auto err = EdsGetChildCount(ref.get_ref(), &listCount); err != EDS_ERR_OK)
         {
-            Poco::Logger::get("directory item").error("Failed to get directory item info (%lu)", err);
-            throw eds_exception("Failed to get directory item info", err, __FUNCTION__);
+            Poco::Logger::get("directory_item")
+                .error("Failed to get directory folder item count (0x%lX)", err);
+            throw eds_exception("Failed to get directory folder item count", err, __FUNCTION__);
         }
 
-        file_size = item.size;
-        format = item.format;
-        name = item.szFileName;
-        is_folder = item.isFolder;
-        group_id = item.groupID;
+        count = listCount;
+    }
+}
+
+impl_directory_ref::~impl_directory_ref() { }
+
+volume_ref::size_type impl_directory_ref::get_directory_count() const
+{
+    if (!is_folder)
+        throw std::logic_error("Not a directory");
+
+    return count;
+}
+
+std::shared_ptr<directory_ref> impl_directory_ref::get_directory_entry(
+    volume_ref::size_type directory_entry_number) const
+{
+    if (!is_folder)
+        throw std::logic_error("Not a directory");
+
+    if (directory_entry_number >= count)
+    {
+        Poco::Logger::get("directory_ref")
+            .error("Directory entry out of range (0x%lX)", directory_entry_number);
+        throw std::out_of_range("Directory entry out of range");
     }
 
-    impl_directory_ref::~impl_directory_ref()
+    EdsDirectoryItemRef dir(nullptr);
+    if (auto err
+        = EdsGetChildAtIndex(ref.get_ref(), static_cast<EdsInt32>(directory_entry_number), &dir);
+        err != EDS_ERR_OK)
     {
+        Poco::Logger::get("directory_ref").error("Failed to get directory entry (0x%lX)", err);
+        throw eds_exception("Failed to get directory entry", err, __FUNCTION__);
     }
+
+    std::shared_ptr<directory_ref> dir_impl = std::make_shared<impl_directory_ref>(dir);
+
+    return dir_impl;
+}
+
 } // namespace implementation
--- camera_interface/volume_ref_impl.cpp
+++ camera_interface/volume_ref_impl.cpp
@@ -13,10 +13,10 @@
 #endif
 #endif
 
-#include <cstdio>
-#include <iostream>
 #include "camera_interface.hpp"
 #include "camera_interface_impl.hpp"
+#include <cstdio>
+#include <iostream>
 
 #include "EDSDK.h"
 
@@ -24,86 +24,92 @@
 
 namespace implementation
 {
-    impl_volume_ref::impl_volume_ref(EdsVolumeRef r) : ref(r), count(0), max_capacity(0), free_space(0), storage_type(storage_type_t::none), access(access_type_t::unknown)
+impl_volume_ref::impl_volume_ref(EdsVolumeRef r)
+    : ref(r)
+    , count(0)
+    , max_capacity(0)
+    , free_space(0)
+    , storage_type(storage_type_t::none)
+    , access(access_type_t::unknown)
+{
+    EdsUInt32 volumeCount = 0;
+    if (auto err = EdsGetChildCount(ref.get_ref(), &volumeCount); err != EDS_ERR_OK)
     {
-        EdsUInt32 volumeCount = 0;
-        if (auto err = EdsGetChildCount(ref.get_ref(), &volumeCount); err != EDS_ERR_OK)
-        {
-            Poco::Logger::get("volume").error("Failed to get volume directory count (%lu)", err);
-            throw eds_exception("Failed to get volume directory count", err, __FUNCTION__);
-        }
-
-        count = volumeCount;
-        EdsVolumeInfo volume;
-
-        if (auto err = EdsGetVolumeInfo(ref.get_ref(), &volume); err != EDS_ERR_OK)
-        {
-            Poco::Logger::get("volume").error("Failed to get volume info (%lu)", err);
-            throw eds_exception("Failed to get volume info", err, __FUNCTION__);
-        }
-
-        max_capacity = volume.maxCapacity;
-        free_space = volume.freeSpaceInBytes;
-        label = volume.szVolumeLabel;
-
-        switch (volume.storageType)
-        {
-        default:
-        case EdsStorageType::kEdsStorageType_Non:
-            storage_type = storage_type_t::none;
-            break;
-        case EdsStorageType::kEdsStorageType_CF:
-            storage_type = storage_type_t::compact_flash;
-            break;
-        case EdsStorageType::kEdsStorageType_SD:
-            storage_type = storage_type_t::sd_card;
-            break;
-        case EdsStorageType::kEdsStorageType_HD:
-            storage_type = storage_type_t::HD;
-            break;
-        case EdsStorageType::kEdsStorageType_CFast:
-            storage_type = storage_type_t::CFast;
-            break;
-        }
-
-        switch (volume.access)
-        {
-        case EdsAccess::kEdsAccess_Read:
-            access = access_type_t::read;
-            break;
-        case EdsAccess::kEdsAccess_Write:
-            access = access_type_t::write;
-            break;
-        case EdsAccess::kEdsAccess_ReadWrite:
-            access = access_type_t::read_write;
-            break;
-        case EdsAccess::kEdsAccess_Error:
-        default:
-            access = access_type_t::unknown;
-        }
+        Poco::Logger::get("volume").error("Failed to get volume directory count (%lu)", err);
+        throw eds_exception("Failed to get volume directory count", err, __FUNCTION__);
     }
 
-    impl_volume_ref::~impl_volume_ref()
+    count = volumeCount;
+    EdsVolumeInfo volume;
+
+    if (auto err = EdsGetVolumeInfo(ref.get_ref(), &volume); err != EDS_ERR_OK)
     {
+        Poco::Logger::get("volume").error("Failed to get volume info (%lu)", err);
+        throw eds_exception("Failed to get volume info", err, __FUNCTION__);
     }
 
-    std::shared_ptr<directory_ref> impl_volume_ref::select_directory(size_type directory_number)
+    max_capacity = volume.maxCapacity;
+    free_space = volume.freeSpaceInBytes;
+    label = volume.szVolumeLabel;
+
+    switch (volume.storageType)
     {
-        if (directory_number >= count)
-        {
-            Poco::Logger::get("volume_ref").error("Directory entry out of range (%lu)", directory_number);
-            throw std::out_of_range("Directory entry out of range");
-        }
-
-        EdsDirectoryItemRef dir(nullptr);
-        if (auto err = EdsGetChildAtIndex(ref.get_ref(), static_cast<EdsInt32>(directory_number), &dir); err != EDS_ERR_OK)
-        {
-            Poco::Logger::get("camera_list").error("Failed to select camera (%lu)", err);
-            throw eds_exception("Failed to select camera", err, __FUNCTION__);
-        }
-
-        std::shared_ptr<directory_ref> dir_impl = std::make_shared<impl_directory_ref>(dir);
-
-        return dir_impl;
+    default:
+    case EdsStorageType::kEdsStorageType_Non:
+        storage_type = storage_type_t::none;
+        break;
+    case EdsStorageType::kEdsStorageType_CF:
+        storage_type = storage_type_t::compact_flash;
+        break;
+    case EdsStorageType::kEdsStorageType_SD:
+        storage_type = storage_type_t::sd_card;
+        break;
+    case EdsStorageType::kEdsStorageType_HD:
+        storage_type = storage_type_t::HD;
+        break;
+    case EdsStorageType::kEdsStorageType_CFast:
+        storage_type = storage_type_t::CFast;
+        break;
     }
+
+    switch (volume.access)
+    {
+    case EdsAccess::kEdsAccess_Read:
+        access = access_type_t::read;
+        break;
+    case EdsAccess::kEdsAccess_Write:
+        access = access_type_t::write;
+        break;
+    case EdsAccess::kEdsAccess_ReadWrite:
+        access = access_type_t::read_write;
+        break;
+    case EdsAccess::kEdsAccess_Error:
+    default:
+        access = access_type_t::unknown;
+    }
+}
+
+impl_volume_ref::~impl_volume_ref() { }
+
+std::shared_ptr<directory_ref> impl_volume_ref::select_directory(size_type directory_number)
+{
+    if (directory_number >= count)
+    {
+        Poco::Logger::get("volume_ref")
+            .error("Directory entry out of range (%lu)", directory_number);
+        throw std::out_of_range("Directory entry out of range");
+    }
+
+    EdsDirectoryItemRef dir(nullptr);
+    if (auto err = EdsGetChildAtIndex(ref.get_ref(), static_cast<EdsInt32>(directory_number), &dir);
+        err != EDS_ERR_OK)
+    {
+        Poco::Logger::get("volume_ref").error("Failed to select directory entry (%lu)", err);
+        throw eds_exception("Failed to select camera", err, __FUNCTION__);
+    }
+
+    std::shared_ptr<directory_ref> dir_impl = std::make_shared<impl_directory_ref>(dir);
+
+    return dir_impl;
+}
 } // namespace implementation
