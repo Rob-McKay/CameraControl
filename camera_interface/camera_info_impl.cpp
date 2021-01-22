22ba7db91b389844aba73eb42100245c2be814c3
Rob McKay <rob@4mation.co.uk>
2021-01-22 15:38:21 +0000

    Made more fields optional / tidied code

--- camera_interface/camera_info_impl.cpp
+++ camera_interface/camera_info_impl.cpp
@@ -18,10 +18,10 @@
 
 #include "EDSDK.h"
 
-#include "Poco/Logger.h"
-#include "Poco/LocalDateTime.h"
 #include "Poco/DateTimeFormat.h"
 #include "Poco/DateTimeFormatter.h"
+#include "Poco/LocalDateTime.h"
+#include "Poco/Logger.h"
 
 #include "properties.hpp"
 
@@ -30,61 +30,73 @@ using namespace std::string_literals;
 
 namespace implementation
 {
-    static const std::string UNKNOWN = "<Unknown>"s;
+static const std::string UNKNOWN = "<Unknown>"s;
 
-    static std::string format_battery_level(EdsUInt32 level)
+static std::string format_battery_level(EdsUInt32 level)
+{
+    if (level != 0xffffffff)
+        return std::to_string(level) + "%";
+    return "AC power";
+}
+
+static std::string format_save_to(EdsUInt32 save_to_code)
+{
+    std::string result;
+
+    if (save_to_code & 1)
+        result += "Memory Card"s;
+
+    if (save_to_code & 2)
     {
-        if (level != 0xffffffff)
-            return std::to_string(level) + "%";
-        return "AC power";
+        if (!result.empty())
+            result += ", "s;
+
+        result += "Host Computer"s;
     }
 
-    static std::string format_save_to(EdsUInt32 save_to_code)
+    return result;
+}
+
+impl_camera_info::impl_camera_info(camera_ref_lock<EdsCameraRef> ref)
+{
+    product_name = (is_property_available(ref.get_ref(), kEdsPropID_ProductName))
+        ? get_camera_property_string(ref.get_ref(), kEdsPropID_ProductName)
+        : UNKNOWN;
+    body_ID_ex = (is_property_available(ref.get_ref(), kEdsPropID_BodyIDEx))
+        ? get_camera_property_string(ref.get_ref(), kEdsPropID_BodyIDEx)
+        : UNKNOWN;
+    owner_name = (is_property_available(ref.get_ref(), kEdsPropID_OwnerName))
+        ? get_camera_property_string(ref.get_ref(), kEdsPropID_OwnerName)
+        : UNKNOWN;
+    maker_name = is_property_available(ref.get_ref(), kEdsPropID_MakerName)
+        ? get_camera_property_string(ref.get_ref(), kEdsPropID_MakerName)
+        : UNKNOWN;
+
+    if (is_property_available(ref.get_ref(), kEdsPropID_UTCTime))
     {
-        std::string result;
-
-        if (save_to_code & 1)
-            result += "Memory Card"s;
-
-        if (save_to_code & 2)
-        {
-            if (!result.empty())
-                result += ", "s;
-
-            result += "Host Computer"s;
-        }
-
-        return result;
+        Poco::Logger::get("camera_info").notice("UTCTime is available but not used");
     }
+    date_time = is_property_available(ref.get_ref(), kEdsPropID_DateTime)
+        ? Poco::DateTimeFormatter::format(
+            get_camera_property_datetime(ref.get_ref(), kEdsPropID_DateTime), "%d-%b-%Y %H:%M:%S"s)
+        : UNKNOWN;
+    firmware_version = get_camera_property_string(ref.get_ref(), kEdsPropID_FirmwareVersion);
+    battery_level
+        = format_battery_level(get_camera_property_int32(ref.get_ref(), kEdsPropID_BatteryLevel));
+    battery_quality = is_property_available(ref.get_ref(), kEdsPropID_BatteryQuality)
+        ? std::to_string(get_camera_property_uint32(ref.get_ref(), kEdsPropID_BatteryQuality))
+        : UNKNOWN;
+    save_to = format_save_to(get_camera_property_uint32(ref.get_ref(), kEdsPropID_SaveTo));
+    current_storage = get_camera_property_string(ref.get_ref(), kEdsPropID_CurrentStorage);
+    current_folder = get_camera_property_string(ref.get_ref(), kEdsPropID_CurrentFolder);
+    lens_status = get_camera_property_uint32(ref.get_ref(), kEdsPropID_LensStatus) != 0;
+    lens_name = get_camera_property_string(ref.get_ref(), kEdsPropID_LensName);
+    artist = get_camera_property_string(ref.get_ref(), kEdsPropID_Artist);
+    copyright = get_camera_property_string(ref.get_ref(), kEdsPropID_Copyright);
 
-    impl_camera_info::impl_camera_info(camera_ref_lock<EdsCameraRef> ref)
-    {
-        product_name = get_camera_property_string(ref.get_ref(), kEdsPropID_ProductName);
-        body_ID_ex = get_camera_property_string(ref.get_ref(), kEdsPropID_BodyIDEx);
-        owner_name = get_camera_property_string(ref.get_ref(), kEdsPropID_OwnerName);
-        maker_name = is_property_available(ref.get_ref(), kEdsPropID_MakerName) ? get_camera_property_string(ref.get_ref(), kEdsPropID_MakerName) : UNKNOWN;
+    available_shots = get_camera_property_uint32(ref.get_ref(), kEdsPropID_AvailableShots);
+}
 
-        if (is_property_available(ref.get_ref(), kEdsPropID_UTCTime))
-        {
-            Poco::Logger::get("camera_info").notice("UTCTime is available but not used");
-        }
-        date_time = is_property_available(ref.get_ref(), kEdsPropID_DateTime) ? Poco::DateTimeFormatter::format(get_camera_property_datetime(ref.get_ref(), kEdsPropID_DateTime), "%d-%b-%Y %H:%M:%S"s) : UNKNOWN;
-        firmware_version = get_camera_property_string(ref.get_ref(), kEdsPropID_FirmwareVersion);
-        battery_level = format_battery_level(get_camera_property_int32(ref.get_ref(), kEdsPropID_BatteryLevel));
-        battery_quality = is_property_available(ref.get_ref(), kEdsPropID_BatteryQuality) ? std::to_string(get_camera_property_uint32(ref.get_ref(), kEdsPropID_BatteryQuality)) : UNKNOWN;
-        save_to = format_save_to(get_camera_property_uint32(ref.get_ref(), kEdsPropID_SaveTo));
-        current_storage = get_camera_property_string(ref.get_ref(), kEdsPropID_CurrentStorage);
-        current_folder = get_camera_property_string(ref.get_ref(), kEdsPropID_CurrentFolder);
-        lens_status = get_camera_property_uint32(ref.get_ref(), kEdsPropID_LensStatus) != 0;
-        lens_name = get_camera_property_string(ref.get_ref(), kEdsPropID_LensName);
-        artist = get_camera_property_string(ref.get_ref(), kEdsPropID_Artist);
-        copyright = get_camera_property_string(ref.get_ref(), kEdsPropID_Copyright);
-
-        available_shots = get_camera_property_uint32(ref.get_ref(), kEdsPropID_AvailableShots);
-    }
-
-    impl_camera_info::~impl_camera_info() noexcept
-    {
-    }
+impl_camera_info::~impl_camera_info() noexcept { }
 
 } // namespace implementation
